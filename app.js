eef123b7dfaabf2f7b1f2c9a13b159a46f7f45f0
Adrian Viquez <aviquez96@gmail.com>
2018-10-22 22:16:48 -0700

    added preliminary authentication; included dependencies, created the user schema, implemented minor passportjs logic

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ var express = require('express'),
     LocalStrategy = require('passport-local'),
     User = require('./models/user')
 
-
 // Constants 
 var port = 3000;
 
@@ -21,6 +20,18 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"))
 seedDb();
+
+// Passport configuration
+app.use(require("express-session")({
+    secret: "This is the secret",
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use( new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 
 // INDEX 
 app.get("/", function (req, res) {
