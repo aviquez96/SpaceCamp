4159d35c3931b29d179114438aac42a10810ab97
Adrian Viquez <aviquez96@gmail.com>
2018-10-23 20:03:03 -0700

    added login page

--- app.js
+++ app.js
@@ -140,6 +140,18 @@ app.post('/register', function(req, res) {
     })
 })
 
+app.get('/login', function(req, res) {
+    res.render('login');
+})
+// handling login logic
+app.post('/login', passport.authenticate("local", 
+    {
+        successRedirect: "/campgrounds", 
+        failureRedirect: "/login"
+    }), function(req, res) {
+
+})
+
 // App server listener
 app.listen(port, console.log("YelpCamp server is up!"));
 
