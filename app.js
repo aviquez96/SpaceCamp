5e62c856e1d857064d95ce19865fb0d56f16b086
Adrian Viquez <aviquez96@gmail.com>
2018-11-18 18:39:00 -0800

    styled the flash messages; added dynamic feature

--- app.js
+++ app.js
@@ -45,6 +45,8 @@ passport.deserializeUser(User.deserializeUser())
 // middleware
 app.use(function (req, res, next) {
     res.locals.currentUser = req.user;
+    res.locals.error = req.flash("error");
+    res.locals.success = req.flash("success");
     next();
 })
 
